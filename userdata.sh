7c144b5ac82f514b12a7cf530350bfe2234651ff
Catherine <ducharme.catherine@tutanota.com>
2024-03-19 10:26:16 -0400

    Added script to deploy Nessus Scanner and link it to cloud.tenable.io

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
- sudo yum update -y   # Update the package repository
- sudo yum install -y <tenable_scanner_package>
+
+# Update package lists
+sudo apt-get update
+
+# Install necessary packages
+sudo apt-get install -y wget
+
+# Download Nessus BYOL package
+wget https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.7.1-ubuntu1804_aarch64.deb
+
+# Install required dependencies for Nessus
+sudo apt-get install -y libssl1.0.0 libuuid1 libxml2
+
+# Install Nessus package
+sudo dpkg -i Nessus-10.7.1-ubuntu1804_aarch64.deb
+
+#Link Nessus BYOL scanner to Tenable Vulnerability Management
+curl -H 'X-Key: 1dbb14c0bfcdb7913d7d891003cc2034b41b279007e20b744de68de6aea49e04' 'https://sensor.cloud.tenable.com/install/scanner?name=PriusOnPrem&groups=PriusGroup' | bash
+
+# Start Nessus service
+sudo systemctl start nessusd
+
+# Enable Nessus service to start on boot
+sudo systemctl enable nessusd
 
 
