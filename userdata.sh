cdfbaf579e730a5756afd4d625ff3c149e235b96
Catherine <ducharme.catherine@tutanota.com>
2024-03-19 16:29:29 -0400

    updated userdata.sh to install Nessus scanner

--- userdata.sh
+++ userdata.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt-get update
+curl --request GET \
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.7.1-amzn2.x86_64.rpm' \
+  --output 'Nessus-10.7.1-amzn2.x86_64.rpm'
+sudo rpm -ivh Nessus-10.7.1-amzn2.x86_64.rpm
+sudo /sbin/service nessusd start
 
-# Install necessary packages
-sudo apt-get install -y wget
-
-# Download Nessus BYOL package
-wget https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.7.1-ubuntu1804_aarch64.deb
-
-# Install required dependencies for Nessus
-sudo apt-get install -y libssl1.0.0 libuuid1 libxml2
-
-# Install Nessus package
-sudo dpkg -i Nessus-10.7.1-ubuntu1804_aarch64.deb
-
-#Link Nessus BYOL scanner to Tenable Vulnerability Management
-curl -H 'X-Key: 1dbb14c0bfcdb7913d7d891003cc2034b41b279007e20b744de68de6aea49e04' 'https://sensor.cloud.tenable.com/install/scanner?name=PriusOnPrem&groups=PriusGroup' | bash
-
-# Start Nessus service
-sudo systemctl start nessusd
-
-# Enable Nessus service to start on boot
-sudo systemctl enable nessusd
-
-
+# After EC2 is up and running, access the Tenable scanner on http://ec2-public-ip:8834
